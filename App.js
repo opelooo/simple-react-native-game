6c489007d8dc755b92b863ed0481251e5c8fa22b
Mathys Jorge Alberino Seilatu <mathys@medinfras.com>
2025-11-19 10:39:59 +0700

    Revert "Update App.js, change deprecated SafeAreaView to react-native-safe-area-context"
    This reverts commit 1e6e57d5220dad7181525813c488fc76fd101082.

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { StyleSheet, ImageBackground, SafeAreaView } from "react-native";
+import { StyleSheet, ImageBackground } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import { SafeAreaView } from "react-native-safe-area-context";
 
