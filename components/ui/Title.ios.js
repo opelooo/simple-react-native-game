fd970d47ca1d2673b881b6734f85ca01f1fedc06
opelooo <mathys.alberino@gmail.com>
2025-11-22 09:12:24 +0700

    Update [89] Writing Platform-specific Code with the Platform API

--- components/ui/Title.ios.js
+++ components/ui/Title.ios.js
@@ -1,4 +1,4 @@
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, Platform } from "react-native";
 
 function Title({ children }) {
   return <Text style={styles.title}>{children}</Text>;
@@ -13,12 +13,12 @@ const styles = StyleSheet.create({
     // fontWeight: 'bold',
     color: "white",
     textAlign: "center",
-    borderWidth: 2,
-    borderColor: "white",
+    // borderWidth: Platform.OS === 'android' ? 2 : 0,
+    // borderWidth: Platform.select({ ios: 0, android: 2 }),
     padding: 12,
     // limit the width of the title to 80% of the screen
     // and set the width to 300px if the screen is smaller than 300px
-    maxWidth: '80%',
-    width: 300
+    maxWidth: "80%",
+    width: 300,
   },
 });
