7d723de07fdfdc98ce80909e64bdf26878bbff3a
opelooo <mathys.alberino@gmail.com>
2025-11-22 09:23:48 +0700

    Update [90] Styling the Status Bar

--- App.js
+++ App.js
@@ -4,8 +4,9 @@ import { LinearGradient } from "expo-linear-gradient";
 // SafeAreaView is deprecated, use react-native-safe-area-context instead
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useFonts } from "expo-font";
-// loading screen package
+// AppLoading is deprecated, use SplashScreen from expo-splash-screen instead
 import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
 
 import StartGameScreen from "./screens/StartGameScreen";
 import GameScreen from "./screens/GameScreen";
@@ -71,20 +72,23 @@ export default function App() {
   }
 
   return (
-    <LinearGradient
-      colors={[Colors.primary700, Colors.accent500]}
-      style={styles.rootScreen}
-      onLayout={onLayoutRootView}
-    >
-      <ImageBackground
-        source={require("./assets/images/background.png")}
-        resizeMode="cover"
+    <>
+      <StatusBar style="light" />
+      <LinearGradient
+        colors={[Colors.primary700, Colors.accent500]}
         style={styles.rootScreen}
-        imageStyle={styles.backgroundImage}
+        onLayout={onLayoutRootView}
       >
-        <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
-      </ImageBackground>
-    </LinearGradient>
+        <ImageBackground
+          source={require("./assets/images/background.png")}
+          resizeMode="cover"
+          style={styles.rootScreen}
+          imageStyle={styles.backgroundImage}
+        >
+          <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
+        </ImageBackground>
+      </LinearGradient>
+    </>
   );
 }
 
